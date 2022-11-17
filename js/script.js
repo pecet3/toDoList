0022b745863203bad87f4f2a6dba40f864e9d4c7
Jakub Pacewicz <pecet3107@gmail.coim>
2022-11-17 23:59:07 +0100

    toggle task done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 {
-    const removeTask = (index) => {
-            tasks.splice(index, 1);
+    const removeTask = (taskIndex) => {
+            tasks.splice(taskIndex, 1);
             render();
         };
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
 
         const tasks = [
             {
@@ -24,7 +28,8 @@
                 htmlString += ` 
             <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
                 ${task.content}
-                <button class = "js-remove">usuń zadanie</button>
+                <button class = "js-remove">usuń</button>
+                <button class = "js-done">zrobione?</button>
             </li>    
             `;
             }
@@ -32,13 +37,21 @@
             document.querySelector(".js-tasks").innerHTML = htmlString;
 
             const removeButtons = document.querySelectorAll(".js-remove");
-
-
             removeButtons.forEach((removeButton, index) => {
                 removeButton.addEventListener("click", () => {
                     removeTask(index);
                 });
             });
+
+            const toggleDoneButtons = document.querySelectorAll(".js-done");
+            
+            toggleDoneButtons.forEach((toggleDoneButton, index) => {
+                toggleDoneButton.addEventListener("click", () => {
+                    toggleTaskDone(index);
+                });
+            });
+
+
         };
         const onFormSubmit = (event) => {
             event.preventDefault();
@@ -54,7 +67,8 @@
 
         const addNewTask = (newTaskContent) => {
             tasks.push({
-                content: newTaskContent
+                content: newTaskContent,
+                done: false,
             });
             render();
         }
